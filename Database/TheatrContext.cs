dea2fb6a4884ea6472bed8d7b0dc72deb646dd50
Kostiantyn Y. <57346019+ENRein@users.noreply.github.com>
2024-06-19 02:11:04 +0300

    Create TheatrContext.cs

---
+++ Database/TheatrContext.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+public class TheatrContext : DbContext {
+    public TheatrContext(DbContextOptions<TheatrContext> options)
+        : base(options) {
+    }
+
+    public DbSet<Scene> Scenes { get; set; }
+    public DbSet<Spectacle> Spectacles { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+}
+
+public class Scene {
+    [Key]
+    public int Id { get; set; }
+    public DateTime DateCreate { get; set; }
+    public string Name { get; set; }
+    public string Data { get; set; }
+}
+
+public class Spectacle {
+    [Key]
+    public int Id { get; set; }
+    public DateTime DateCreate { get; set; }
+    public int SceneId { get; set; } 
+    public string Name { get; set; }
+    public string Desc { get; set; }
+    public DateTime DatePublic { get; set; }
+
+    public virtual Scene Scene { get; set; }
+}
+
+public class Ticket {
+    [Key]
+    public int Id { get; set; }
+    public int SpectacleId { get; set; }
+    public string TransactionId { get; set; }
+    public DateTime DateCreate { get; set; }
+    public string Seat { get; set; }
+
+    public virtual Spectacle Spectacle { get; set; } 
+    public virtual Transaction Transaction { get; set; } 
+}
+
+public class Transaction {
+    [Key]
+    public string OrderId { get; set; } // UUID or int
+    public decimal Amount { get; set; }
+    public string Status { get; set; }
+}
